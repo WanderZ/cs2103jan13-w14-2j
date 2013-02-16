a727226d10e19cb278fca7e77f05f496c2674849
kohzk <kohzk@comp.nus.edu.sg>
2013-02-16 19:59:04 +0800

    test clone

--- EzXpns/src/ezxpns/Main.java
+++ EzXpns/src/ezxpns/Main.java
@@ -1,24 +1,33 @@
 package ezxpns;
 
+
 import java.awt.EventQueue;
 
+
 import ezxpns.GUI.MainWindow;
+
 
 public class Main {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MainWindow window = new MainWindow();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	
+    public static void main(String[] args) {
+		
+        EventQueue.invokeLater(new Runnable() {
+			
+            public void run() {
+				
+                try {
+					
+                    MainWindow window = new MainWindow();
+
+                } catch (Exception e) {e.printStackTrace();
+}
+            }
+        });
+    }
+
 
 }
