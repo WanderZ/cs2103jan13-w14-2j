7e1c9880fa293bcca226214841df5b124a9f15d4
ytz <tingzhe@gmail.com>
2013-02-23 15:21:31 +0800

    Added Report.java and ReportCategory.java

---
+++ EzXpns/src/ezxpns/data/ReportCategory.java
@@ -0,0 +1,12 @@
+package ezxpns.data;
+
+public class ReportCategory {
+	
+	private String category;
+	private int frequency = 0;
+	private double amount = 0;
+	private double percentage = 0;
+	private double amtPerFreq = 0;
+	
+
+}
