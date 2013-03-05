aa7a96bd12424bd276b0c61010e11e64e9334bf7
kohzk <kohzk@comp.nus.edu.sg>
2013-03-05 23:08:15 +0800

    updates to layouting of the forms. Added the New Income Tab coding

--- EzXpns/src/ezxpns/GUIMain.java
+++ EzXpns/src/ezxpns/GUIMain.java
@@ -2,28 +2,27 @@ package ezxpns;
 import ezxpns.GUI.*;
 import java.awt.EventQueue;
 
-public class Main {
-
-	/**
-	 * @param args
-	 */
+public class GUIMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					/* Essentially the following code starts the GUI */
+					/* Main GUI */
 					MainScreen main = new MainScreen();
 					main.showScreen();
-					/* The following is for testing, GUI Trial and Error */
-					FrameRecord recordMgr = new FrameRecord();
+					
+					/* Record Frame GUI - For testing */
+					RecordFrame recordMgr = new RecordFrame();
 					recordMgr.showScreen();
+					
+					/* Search Frame GUI - For testing */
+					SearchFrame searchMgr = new SearchFrame();
+					searchMgr.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
-		
 	}
-
 }
