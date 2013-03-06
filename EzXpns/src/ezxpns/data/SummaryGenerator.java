ddc67934f86739f3ca3b84964ffb102a673ab8b3
ytz <tingzhe@gmail.com>
2013-03-06 18:19:40 +0800

    Added SummaryDetails.java. Need additional methods from interface for computations. Otherwise mostly done.

--- EzXpns/src/ezxpns/data/SummaryGenerator.java
+++ EzXpns/src/ezxpns/data/SummaryGenerator.java
@@ -6,11 +6,12 @@ import java.util.Vector;
 import ezxpns.data.ReportGenerator.DataProvider;
 import ezxpns.data.records.ExpenseRecord;
 import ezxpns.data.records.IncomeRecord;
+import ezxpns.data.records.Record;
 import ezxpns.util.Pair;
 
 /**
  * Generate a Summary Object 
- * @author yan
+ * @author tingzhe
  * 
  */
 
@@ -32,5 +33,23 @@ public class SummaryGenerator {
 	public SummaryGenerator(DataProvider data) {
 		this.data = data;
 	}
+	
+	public Summary generateSummary(){
+		SummaryDetails myAllTime = computeSummaryDetails(??); // argument is Pair<Vector<ExpenseRecord>, Vector<IncomeRecord>>
+		SummaryDetails myYearly = computeSummaryDetails(??);
+		SummaryDetails myMonthly = computeSummaryDetails(??);
+		SummaryDetails myToday = computeSummaryDetails(??);
+		
+		return new Summary(myAllTime, myYearly, myMonthly, myToday);
+	}
+
+	private SummaryDetails computeSummaryDetails(Pair<Vector<ExpenseRecord>, Vector<IncomeRecord>> pair) {
+		double expense = Record.sumAmount(pair.getLeft());
+		double income = Record.sumAmount(pair.getRight());
+		
+		return new SummaryDetails(income, expense);
+	}
+
+	
 
 }
