71a4388351584b98467aa73af955d2432982a74d
yyjhao <yyjhao@localhost>
2013-03-07 20:23:55 +0800

    Change search request

--- EzXpns/src/ezxpns/GUI/SearchRequest.java
+++ EzXpns/src/ezxpns/GUI/SearchRequest.java
@@ -1,18 +1,65 @@
 package ezxpns.GUI; // This is a temporal storing location for compilation and testing. TO BE MOVED
 
 import java.util.Date;
+import ezxpns.util.*;
+import ezxpns.data.records.*;
 
 /**
- * This class contains the fields that are vital for retrieving records from the storage
- * Can probably be extended to facilitate the advanced search
+ * Contains search query parameters
+ * May support multiple queries but not handled in the master class for now.
  */
 public class SearchRequest {
+	public enum RecordType {
+		EXPENSE, INCOME, BOTH;
+	}
 	
-	private int type;
+	private RecordType type = RecordType.BOTH;
+	
 	private String name;
-	private String category; // Maybe payment can be included
-	private Date start, end;
 	
-	public SearchRequest() {}
+	public RecordType getType() {
+		return type;
+	}
+
+	public void setType(RecordType type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public boolean isMultiple() {
+		return multiple;
+	}
+
+	private Category category;
+	
+	private Pair<Date, Date> dateRange;
+	
+	public Pair<Date, Date> getDateRange() {
+		return dateRange;
+	}
+
+	private boolean multiple = false;
+	
+	// no empty request allowed!
+	protected SearchRequest(){}
+	
+	public SearchRequest(String name){
+		this.name = name;
+	}
+	
+	public SearchRequest(Pair<Date, Date> dateRange){
+		this.dateRange = dateRange;
+	}
+	
+	public SearchRequest(Category category){
+		this.category = category;
+	}
 	
 }
