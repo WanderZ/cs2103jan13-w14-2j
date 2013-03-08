f15d2514c9dd4819424251c7641af56dada14d48
Yao Yujian yyjhao <Yao Yujian yyjhao@gmail.com>
2013-03-08 14:42:23 +0800

    stub unit test

---
+++ EzXpns/src/test/DataQueryTest.java
@@ -0,0 +1,74 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DataQueryTest {
+
+	@Test
+	public void testIsUpdated() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaved() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAfterDeserialize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateRecord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveRecord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddNewRecord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveCategory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateCategory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddNewCategory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCategory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRecordsByCategoryInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRecordsByStringInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRecordsByDateDateIntBoolean() {
+		fail("Not yet implemented");
+	}
+
+}
