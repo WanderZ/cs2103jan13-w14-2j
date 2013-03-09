62290261b4ed3fcfcf89c8ac78d11f9797bef894
kohzk <kohzk@comp.nus.edu.sg>
2013-03-09 21:53:15 +0800

    Merge with Report

--- EzXpns/src/ezxpns/Ezxpns.java
+++ EzXpns/src/ezxpns/Ezxpns.java
@@ -5,6 +5,7 @@ import ezxpns.data.records.Category;
 import ezxpns.data.records.ExpenseRecord;
 import ezxpns.data.records.IncomeRecord;
 import ezxpns.data.records.Record;
+import ezxpns.data.records.RecordManager.RecordUpdateException;
 import ezxpns.util.Pair;
 import ezxpns.GUI.*;
 import java.awt.EventQueue;
@@ -55,6 +56,10 @@ public class Ezxpns implements
 					recordMgrE.showScreen();
 					*/
 					
+					/* Test Report Frame*/
+					ReportFrame tzTestFrame = new ReportFrame();
+					tzTestFrame.showScreen();
+					
 					/*
 					SearchFrame searchMgr = new SearchFrame();
 					searchMgr.setVisible(true);
@@ -86,11 +91,11 @@ public class Ezxpns implements
 		store.save();
 	}
 	
-	public void addRecord(ExpenseRecord r){
+	public void addRecord(ExpenseRecord r) throws RecordUpdateException{
 		data.expenses().addNewRecord(r);
 	}
 	
-	public void addRecord(IncomeRecord r){
+	public void addRecord(IncomeRecord r) throws RecordUpdateException{
 		data.incomes().addNewRecord(r);
 	}
 
