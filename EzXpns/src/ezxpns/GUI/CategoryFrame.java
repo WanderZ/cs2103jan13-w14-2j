4d9fd1f7f79b211b93859862f30a1b1e56ac3351
Yao Yujian yyjhao <Yao Yujian yyjhao@gmail.com>
2013-03-14 18:00:53 +0800

    MVC for categories

--- EzXpns/src/ezxpns/GUI/CategoryFrame.java
+++ EzXpns/src/ezxpns/GUI/CategoryFrame.java
@@ -1,9 +1,10 @@
 package ezxpns.GUI;
 
 import javax.swing.JFrame;
+import java.util.*;
 
 import ezxpns.data.*;
-import javax.swing.JList;
+import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import ezxpns.data.records.*;
@@ -30,6 +31,8 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.event.ListSelectionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 /**
  * Window for users to manage their targets and / or categories [?]
@@ -39,18 +42,26 @@ public class CategoryFrame extends JFrame {
 	
 	private TargetManager targetMgr;
 	private CategoryHandlerInterface excats, incats;
+	private CategoryModel exmo, inmo;
 	
-	JList exlist, inlist;
+	private JList exlist, inlist;
+	
 	private JTextField targetAmountField;
 	private JTextField exnameField;
 	private JTextField textField_2;
+	private Category addNew;
 	
 	private Category curCat;
 	private Target curTar;
 	
+	private JButton removeExBtn, changeExBtn;
+	
 	public CategoryFrame(CategoryHandlerInterface excats,
 			CategoryHandlerInterface incats, TargetManager targetMgrRef) {
 		super(); // new JFrame();
+		addNew = new Category(-1, "Add a new category...");
+		exmo = new CategoryModel(excats, addNew);
+		inmo = new CategoryModel(incats, addNew);
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowOpened(WindowEvent arg0) {
@@ -124,7 +135,12 @@ public class CategoryFrame extends JFrame {
 		panel.add(targetAmountField, gbc_targetAmountField);
 		targetAmountField.setColumns(10);
 		
-		JButton changeExBtn = new JButton("Change");
+		changeExBtn = new JButton("Change");
+		changeExBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				modifyEx();
+			}
+		});
 		GridBagConstraints gbc_changeExBtn = new GridBagConstraints();
 		gbc_changeExBtn.insets = new Insets(0, 0, 5, 5);
 		gbc_changeExBtn.anchor = GridBagConstraints.NORTHWEST;
@@ -132,18 +148,17 @@ public class CategoryFrame extends JFrame {
 		gbc_changeExBtn.gridy = 4;
 		panel.add(changeExBtn, gbc_changeExBtn);
 		
-		JButton btnRemove_1 = new JButton("Remove");
-		GridBagConstraints gbc_btnRemove_1 = new GridBagConstraints();
-		gbc_btnRemove_1.insets = new Insets(0, 0, 5, 0);
-		gbc_btnRemove_1.gridx = 2;
-		gbc_btnRemove_1.gridy = 4;
-		panel.add(btnRemove_1, gbc_btnRemove_1);
-		
-		JButton btnNew_1 = new JButton("New");
-		GridBagConstraints gbc_btnNew_1 = new GridBagConstraints();
-		gbc_btnNew_1.gridx = 2;
-		gbc_btnNew_1.gridy = 9;
-		panel.add(btnNew_1, gbc_btnNew_1);
+		removeExBtn = new JButton("Remove");
+		removeExBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				removeEx();
+			}
+		});
+		GridBagConstraints gbc_removeExBtn = new GridBagConstraints();
+		gbc_removeExBtn.insets = new Insets(0, 0, 5, 0);
+		gbc_removeExBtn.gridx = 2;
+		gbc_removeExBtn.gridy = 4;
+		panel.add(removeExBtn, gbc_removeExBtn);
 		
 		JPanel panel_2 = new JPanel();
 		tabbedPane.addTab("Income", null, panel_2, null);
@@ -194,32 +209,87 @@ public class CategoryFrame extends JFrame {
 		gbc_btnRemove.gridx = 2;
 		gbc_btnRemove.gridy = 4;
 		panel_3.add(btnRemove, gbc_btnRemove);
-		
-		JButton btnNew = new JButton("New");
-		GridBagConstraints gbc_btnNew = new GridBagConstraints();
-		gbc_btnNew.gridx = 2;
-		gbc_btnNew.gridy = 9;
-		panel_3.add(btnNew, gbc_btnNew);
 		targetMgr = targetMgrRef;
+		
+		exlist.setModel(exmo);
+		inlist.setModel(inmo);
 		
 	}
 	
 	private void loadExList(){
-		exlist.setListData(excats.getAllCategories().toArray());
+//		List<Category> cats = excats.getAllCategories();
+//		cats.add(addNew);
+//		exlist.setListData(cats.toArray());
+//		exlist.setSelectedIndex(0);
+		exmo.update();
 	}
 	
 	private void loadInList(){
-		inlist.setListData(incats.getAllCategories().toArray());
+//		List<Category> cats = incats.getAllCategories();
+//		cats.add(addNew);
+//		inlist.setListData(cats.toArray());
+//		inlist.setSelectedIndex(0);
+		inmo.update();
 	}
 
 	private void updateExDisplay(Category cat){
 		curCat = cat;
-		exnameField.setText(cat.getName());
-		curTar = targetMgr.getTarget(cat);
-		if(curTar == null){
-			targetAmountField.setText("No target set");
+		if(cat == null){
+			return;
+		}
+		if(cat != addNew){
+			exnameField.setText(cat.getName());
+			curTar = targetMgr.getTarget(cat);
+			if(curTar == null){
+				targetAmountField.setText("No target set");
+			}else{
+				targetAmountField.setText("" + curTar.getTarget());
+			}
+			changeExBtn.setText("Change");
+			removeExBtn.setEnabled(true);
 		}else{
-			targetAmountField.setText("" + curTar.getTarget());
+			exnameField.setText("Pick a name");
+			targetAmountField.setText("Set a target");
+			changeExBtn.setText("Add");
+			removeExBtn.setEnabled(false);
+		}
+	}
+	
+	private boolean validateName(String name){
+		return !name.contains(" ") && name.length() > 2 && name.length() < 20;
+	}
+	
+	private boolean validateTarget(String targetString){
+		double d;
+		try{
+			d = Double.parseDouble(targetString);
+			return d > 10;
+		}catch(NumberFormatException e){
+			return false;
+		}
+	}
+	
+	private void modifyEx(){
+		if(validateName(exnameField.getText())){
+			if(curCat == addNew){
+				Category cat = excats.addNewCategory(new Category(exnameField.getText()));
+				if(validateTarget(targetAmountField.getText())){
+					targetMgr.setTarget(cat, Double.parseDouble(targetAmountField.getText()));
+				}
+				exmo.update();
+				exlist.setSelectedIndex(0);
+			}
+		}
+	}
+	
+	private void removeEx(){
+		String message = "Are you sure you want to remove this category?\n" +
+			    		"All records under this category will have an undefined category!";
+		if(JOptionPane.showConfirmDialog(this, message, "what?!",
+				JOptionPane.WARNING_MESSAGE, JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION){
+			excats.removeCategory(curCat.getID());
+			exmo.update();
+			exlist.setSelectedIndex(0);
 		}
 	}
 }
