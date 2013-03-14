bf8921bafaa63c7f9528950ec6fcf7ec1cb7424d
Yao Yujian yyjhao <Yao Yujian yyjhao@gmail.com>
2013-03-14 11:22:52 +0800

    Add a data generator

--- EzXpns/src/ezxpns/GUI/RecordFrame.java
+++ EzXpns/src/ezxpns/GUI/RecordFrame.java
@@ -130,13 +130,9 @@ public class RecordFrame extends JFrame implements ActionListener {
 				
 				// Check if it is a recurring record
 				// do the necessary to ensure that EzXpns knows it. 
-				
-				// Display confirmation?
 			}
 		}
 		if(this.panOpt.getCancelBtn() == e.getSource()) {
-			// Cancel button has been invoked. 
-			// Invoke confirmation? - will it be retarded to invoke confirmation?
 			this.dispose();
 		}
 	}
