b8b5a0b81ac5dae692daeedbe3c3e9573330ec8f
kohzk <kohzk@comp.nus.edu.sg>
2013-03-17 18:42:59 +0800

    UIControl - removed depreciated method showRecwin() use the one with the parameters instead
    Minor Tweaks to search and new record UI

---
+++ EzXpns/src/ezxpns/data/Storable.java
@@ -0,0 +1,32 @@
+package ezxpns.data;
+
+public abstract class Storable {
+	private transient boolean updated;
+	/**
+	 * @return if the object has been updated and needs to be stored again
+	 */
+	public boolean isUpdated(){
+		return updated;
+	}
+	
+	/**
+	 * Tells the object that it has been stored
+	 */
+	public void saved(){
+		updated = false;
+	}
+	
+	/**
+	 * Mark the object as updated so that the storage manager
+	 * will attempt to save the data
+	 */
+	protected void markUpdate(){
+		updated = true;
+	}
+	
+	/**
+	 * Optional method to populate transient attributes after deserializing data
+	 * from json.
+	 */
+	public void afterDeserialize(){ }
+}
