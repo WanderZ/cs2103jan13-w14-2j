a1ae282fc85bf7e4a8c89c023106e7f93c83ba19
kohzk <kohzk@comp.nus.edu.sg>
2013-03-17 22:26:44 +0800

    Merge with frontend

---
+++ EzXpns/src/ezxpns/Main.java
@@ -0,0 +1,15 @@
+package ezxpns;
+
+import ezxpns.GUI.*;
+import ezxpns.data.records.*;
+import java.util.*;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Ezxpns master = new Ezxpns();
+	}
+}
