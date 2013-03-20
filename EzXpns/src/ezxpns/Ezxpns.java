4043c087f9e4c5527468633aadac02a8bbf733e4
Yao Yujian yyjhao <Yao Yujian yyjhao@gmail.com>
2013-03-20 12:57:22 +0800

    merge with default

--- EzXpns/src/ezxpns/Ezxpns.java
+++ EzXpns/src/ezxpns/Ezxpns.java
@@ -14,6 +14,7 @@ import ezxpns.data.records.RecordManager.RecordUpdateException;
 import ezxpns.data.records.SearchRequest.RecordType;
 import ezxpns.util.Pair;
 import ezxpns.GUI.*;
+
 import java.awt.EventQueue;
 import java.util.*;
 
