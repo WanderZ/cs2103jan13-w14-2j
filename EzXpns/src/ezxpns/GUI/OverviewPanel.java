0ee77b4d0c808323c62f9013b40a79e2fe6afac6
ytz <tingzhe@gmail.com>
2013-03-25 01:11:00 +0800

    Added JComboBox to SearchFrame.

---
+++ EzXpns/src/ezxpns/GUI/OverviewPanel.java
@@ -0,0 +1,193 @@
+package ezxpns.GUI;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.text.DecimalFormat;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
+
+import net.miginfocom.swing.MigLayout;
+import ezxpns.data.SummaryDetails;
+import ezxpns.data.SummaryGenerator;
+import ezxpns.data.SummaryGenerator.SummaryType;
+import javax.swing.JSeparator;
+
+/**
+ * This is the overview a.k.a summary panel that display the user's balance, in
+ * and out cash flow.
+ */
+@SuppressWarnings("serial")
+public class OverviewPanel extends JPanel {
+	private SummaryType[] summaryTab = { SummaryType.TODAY, SummaryType.MONTH,
+			SummaryType.YEAR, SummaryType.ALLTIME };
+	private int index = 1;
+
+	private SummaryGenerator sumGen;
+	private JLabel lblTimeFrame;
+	private JButton buttonBack;
+	private JButton buttonNext;
+	private JLabel lblBalance;
+	private JLabel lblIncomeNumber;
+	private JLabel lblExpenseNumber;
+
+	DecimalFormat df = new DecimalFormat("#.##");
+
+	public OverviewPanel(SummaryGenerator sumGenRef) {
+		super();
+		setBackground(new Color(255, 255, 255));
+
+		this.sumGen = sumGenRef;
+
+		JPanel panelTime = new JPanel();
+		panelTime.setBackground(Color.WHITE); // set color for buttons and timeline
+		
+		JLabel lblSummary = new JLabel("Summary");
+		lblSummary.setForeground(new Color(192, 192, 192));
+		lblSummary.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		
+		JPanel panelBigBalance = new JPanel();
+		panelBigBalance.setBackground(new Color(255, 255, 255));
+		
+		JPanel panelSub = new JPanel();
+		panelSub.setBackground(new Color(255, 255, 255));
+		
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(panelTime, GroupLayout.PREFERRED_SIZE, 423, GroupLayout.PREFERRED_SIZE)
+						.addComponent(panelSub, GroupLayout.PREFERRED_SIZE, 424, GroupLayout.PREFERRED_SIZE)
+						.addComponent(panelBigBalance, GroupLayout.PREFERRED_SIZE, 428, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblSummary))
+					.addContainerGap(16, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(8)
+					.addComponent(lblSummary)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panelBigBalance, GroupLayout.PREFERRED_SIZE, 121, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panelSub, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panelTime, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(28, Short.MAX_VALUE))
+		);
+		
+		// panelBigBalance
+		// ===============
+		panelBigBalance.setLayout(new MigLayout("", "0[125][300]", "0[70][40]0"));
+		
+		JLabel lblImage = new JLabel("image");
+		panelBigBalance.add(lblImage);
+		
+		lblBalance = new JLabel("Balance");
+		lblBalance.setFont(new Font("Lucida Grande", Font.PLAIN, 50));
+		panelBigBalance.add(lblBalance, "cell 1 0 1 2, align center");
+		
+		JLabel lblStatus = new JLabel("status");
+		panelBigBalance.add(lblStatus, "cell 0 1");
+		
+		// panelSub
+		// ========
+		panelSub.setLayout(new MigLayout("", "[80][10][80]", "0[20][20]0"));
+		
+		JLabel lblIncome = new JLabel("Income");
+		panelSub.add(lblIncome, "cell 0 0, align center");
+		
+		JLabel lblExpense = new JLabel("Expense");
+		panelSub.add(lblExpense, "cell 2 0,alignx center");
+		
+		JSeparator separator = new JSeparator();
+		separator.setOrientation(SwingConstants.VERTICAL);
+		panelSub.add(separator, "cell 1 0 1 2, growy");
+		
+		lblIncomeNumber = new JLabel("income number");
+		panelSub.add(lblIncomeNumber, "cell 0 1, align center");
+		
+		lblExpenseNumber = new JLabel("expense number");
+		panelSub.add(lblExpenseNumber, "cell 2 1,alignx center");
+
+		
+		
+		// TIME PANEL
+		// ===========
+		panelTime.setLayout(new BorderLayout(0, 0));
+
+		// Back Button
+		buttonBack = new JButton("<");
+		buttonBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		buttonBack.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				index -= 1;
+				if (index < 0)
+					index = 0;
+				getSummaryData();
+			}
+		});
+		panelTime.add(buttonBack, BorderLayout.WEST);
+
+		// Next Button
+		buttonNext = new JButton(">");
+		buttonNext.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				index += 1;
+				if (index > 3)
+					index = 3;
+				getSummaryData();
+			}
+		});
+		panelTime.add(buttonNext, BorderLayout.EAST);
+		
+		// Time Frame
+		lblTimeFrame = new JLabel("");
+		lblTimeFrame.setHorizontalAlignment(SwingConstants.CENTER);
+		panelTime.add(lblTimeFrame, BorderLayout.CENTER);
+		
+		// Get Data
+		// =========
+		getSummaryData();
+		
+		setLayout(groupLayout);
+
+	}
+
+	private void getSummaryData() {
+		SummaryDetails mySummaryDetails = sumGen
+				.getSummaryDetails(summaryTab[index]);
+
+		//lblBalance.setText("Balance:\t" + df.format(mySummaryDetails.getBalance()));
+		lblIncomeNumber.setText(df.format(mySummaryDetails.getIncome()));
+		lblExpenseNumber.setText(df.format(mySummaryDetails.getExpense()));
+		lblTimeFrame.setText(mySummaryDetails.getSummaryType().getName());
+		lblBalance.setText(df.format(mySummaryDetails.getBalance()));
+		
+		if (mySummaryDetails.getBalance() > 0)
+			lblBalance.setForeground(Color.GREEN.darker());
+		else if (mySummaryDetails.getBalance() < 0)
+			lblBalance.setForeground(Color.RED.darker());
+	}
+	
+	public void updateOverview(){
+		getSummaryData();
+	}
+}
---
+++ EzXpns/src/ezxpns/GUI/SearchFrame.java
@@ -0,0 +1,293 @@
+package ezxpns.GUI;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+import net.miginfocom.swing.MigLayout;
+import ezxpns.data.records.Category;
+import ezxpns.data.records.CategoryHandler;
+import ezxpns.data.records.Record;
+import ezxpns.data.records.SearchHandler;
+import ezxpns.data.records.SearchRequest;
+import ezxpns.util.Pair;
+
+/**
+ * The window to handle the searching and querying needs of the user
+ */
+@SuppressWarnings("serial")
+public class SearchFrame extends JFrame implements ActionListener {
+	
+	public final int DEFAULT_WIDTH = 600;
+	public final int DEFAULT_HEIGHT = 400;
+	
+	/** The handler object that implemented SearchHandler & CategoryHandler interface */
+	private SearchHandler handler;
+	private CategoryHandler inCatHandRef;
+	private CategoryHandler exCatHandRef;
+
+	
+	// 2 main panels, the top (querying time frame) and the bottom (results, content)
+	private SearchFormPanel panForm;
+	private SearchBtnPanel panBtns;
+	private JScrollPane panResult;
+	private RecordListView list;
+	
+	/**
+	 * To create a new Search Window
+	 * @param handlerRef the reference to the SearchHandler object, catHandRef to CategoryHandler
+	 */
+	public SearchFrame(SearchHandler handlerRef, RecordListView li, CategoryHandler inCatHandRef, CategoryHandler exCatHandRef) {
+		super();
+		this.init();
+		this.setLayout(new BorderLayout(25, 25));
+		this.handler = handlerRef;
+		
+		JPanel panCtrls = new JPanel();
+		panCtrls.setLayout(new BorderLayout());
+		
+		panForm = new SearchFormPanel(inCatHandRef, exCatHandRef);
+		panCtrls.add(panForm, BorderLayout.CENTER);
+		
+		panBtns = new SearchBtnPanel(this);
+		panCtrls.add(panBtns, BorderLayout.EAST);
+		
+		this.add(panCtrls, BorderLayout.NORTH);
+		
+		list = li;
+		list.setPreferredScrollableViewportSize(new Dimension(100, 200));
+		this.panResult = new JScrollPane(list);
+		this.add(this.panResult, BorderLayout.CENTER);
+	}
+	
+	/** to initialize the components of this frame */
+	private void init() {
+		this.setTitle("EzXpns - Search");
+		this.setBounds(0, 0, DEFAULT_WIDTH, DEFAULT_HEIGHT); /*x coordinate, y coordinate, width, height*/
+		this.setLocationRelativeTo(null); // Set to start in the central area of the screen
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		if(event.getSource() == panBtns.getSearchBtn()) {
+			// User invoked search
+			
+			StringBuilder request = new StringBuilder();
+			request.append(panForm.getNameField().getText().trim());
+			if(!request.equals("")) {
+				this.search(new SearchRequest(request.toString())); // Search by Name
+				return;
+			}
+			
+			// Name field is empty!
+			request.append(panForm.getCatField().getSelectedItem().toString().trim()); // I changed some stuff, how sure how to get search working (tingzhe)
+			System.out.println(request.append(panForm.getCatField().getSelectedItem().toString().trim()));
+
+			if(!request.equals("")) {
+				this.search(new SearchRequest(new Category(request.toString()))); // Search by Category
+				return;
+			}	
+			// No Category entered!
+			
+			try {
+				Pair<Date, Date> dateRange = new Pair<Date, Date>(panForm.getStartDate(), panForm.getEndDate());
+				this.search(new SearchRequest(dateRange)); // Search by Date range
+			}
+			catch(Exception err) {
+				return;
+			}
+		}
+	}
+	
+	/** 
+	 * Search the store data for the relevant records
+	 * @param request SearchRequest object containing the query
+	 */
+	private void search(SearchRequest request) {
+		List<Record> results = handler.search(request);
+		list.show(results);
+	}
+}
+
+@SuppressWarnings("serial")
+class SearchFormPanel extends JPanel {
+	
+	// CategoryHandler Reference for Category JComboBox
+	private CategoryHandler inCatHandRef;
+	private CategoryHandler exCatHandRef;
+	
+	private JLabel lblName, lblTitle, lblCat, lblDate, lblToDate;
+	private JTextField txtName;
+	private JComboBox txtCat;
+	private JFormattedTextField txtStart, txtEnd;
+	private final Font FORM_FONT = new Font("Segoe UI", 0, 14); // #Font
+	
+	public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yy");
+	
+	public SearchFormPanel(CategoryHandler inCatHandRef, CategoryHandler exCatHandRef) {
+		this.inCatHandRef = inCatHandRef; // reference
+		this.exCatHandRef = exCatHandRef; // reference
+		
+		this.setLayout(new MigLayout("insets 15", "[left]10%[]", ""));
+		this.add(this.getTitleLabel(), "span, wrap");
+		
+		this.add(this.getNameLabel(), "span 2");
+		this.add(this.getNameField(), "span, wrap");
+		
+		this.add(this.getCatLabel(), "span 2");
+		this.add(this.getCatField(), "span, wrap");
+		
+		this.add(this.getDateLabel(), "span 2");
+		this.add(this.getDateField(), "split 3");
+		this.add(this.getToDateLabel());
+		this.add(this.getToDateField());
+	}
+	
+	private JLabel getTitleLabel() {
+		if(lblTitle == null) {
+			lblTitle = new JLabel("Search");
+			lblTitle.setFont(new Font("Segoe UI", 0, 24)); // #Font
+		}
+		return lblTitle;
+	}
+	
+	private JLabel getNameLabel() {
+		if(lblName == null) {
+			lblName = new JLabel("Name");
+			lblName.setFont(FORM_FONT); // #Font
+		}
+		return lblName;
+	}
+	
+	public JTextField getNameField() {
+		if(txtName == null) {
+			txtName = new JTextField("");
+			txtName.setFont(FORM_FONT); // #Font
+			txtName.setPreferredSize(new Dimension(230, 32));
+		}
+		return txtName;
+	}
+	
+	private JLabel getCatLabel() {
+		if(lblCat == null) {
+			lblCat = new JLabel("Category");
+			lblCat.setFont(FORM_FONT); // #Font
+		}
+		return lblCat;
+	}
+	
+	public JComboBox getCatField() {
+		if(txtCat == null) {
+			//txtCat = new JTextField("");
+			Object[] myInCatList = new Category[inCatHandRef.getAllCategories().size()];
+			myInCatList = inCatHandRef.getAllCategories().toArray();
+			txtCat = new JComboBox(myInCatList);
+			Object[] myExCatList = new Category[exCatHandRef.getAllCategories().size()];
+			myExCatList = exCatHandRef.getAllCategories().toArray();
+			for (int i = 0; i < exCatHandRef.getAllCategories().size(); i++)
+				txtCat.addItem(myExCatList[i]);
+			txtCat.setFont(FORM_FONT); // #Font
+			txtCat.setPreferredSize(new Dimension(230, 32));
+		}
+		return txtCat;
+	}
+
+	private JLabel getDateLabel() {
+		if(lblDate == null) {
+			lblDate = new JLabel("Date");
+			lblDate.setFont(FORM_FONT); // #Font
+		}
+		return lblDate;
+	}
+	
+	private JLabel getToDateLabel() {
+		if(lblToDate == null) {
+			lblToDate = new JLabel(" To ");
+			lblToDate.setFont(FORM_FONT); // #Font
+			
+		}
+		return lblToDate;
+	}
+	
+	private JFormattedTextField getDateField() {
+		if(txtStart == null) {
+			txtStart = new JFormattedTextField(DATE_FORMAT);
+			txtStart.setFont(FORM_FONT); // #Font
+			txtStart.setValue(new Date());
+			txtStart.setPreferredSize(new Dimension(100, 32));
+			txtStart.setHorizontalAlignment(JTextField.CENTER);
+		}
+		return txtStart;
+	}
+	
+	public Date getStartDate() {
+		return (Date)txtStart.getValue();
+	}
+	
+	private JFormattedTextField getToDateField() {
+		if(txtEnd == null) {
+			txtEnd = new JFormattedTextField(DATE_FORMAT);
+			txtEnd.setFont(FORM_FONT); // #Font
+			txtEnd.setValue(new Date());
+			txtEnd.setPreferredSize(new Dimension(100, 32));
+			txtEnd.setHorizontalAlignment(JTextField.CENTER);
+		}
+		return txtEnd;
+	}
+	
+	public Date getEndDate() {
+		return (Date) txtEnd.getValue();
+	}
+}
+
+/**
+ * Panel to containing the buttons for functionality
+ */
+@SuppressWarnings("serial")
+class SearchBtnPanel extends JPanel {
+	
+	private JButton btnSearch;
+	private final Font BTN_FONT = new Font("Segoe UI", 0, 24);
+	
+	/**
+	 * Constructor to get a panel for the buttons
+	 * @param listener
+	 */
+	public SearchBtnPanel(ActionListener listener) {
+		super();
+		BoxLayout loBtn = new BoxLayout(this, BoxLayout.Y_AXIS);
+		this.setLayout(loBtn);
+		
+		// Layout to make it fill the width completely, and glue to the base
+
+		this.add(this.getSearchBtn());
+		this.add(Box.createVerticalGlue());
+		
+		this.btnSearch.addActionListener(listener);
+	}
+	
+	public JButton getSearchBtn() {
+		if(btnSearch == null) {
+			btnSearch = new JButton("Find");
+			btnSearch.setFont(BTN_FONT);
+		}
+		return btnSearch;
+	}
+}
