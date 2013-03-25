f57a08019bb32e569ad97eac1cd5a9d58206ec9c
kohzk <kohzk@comp.nus.edu.sg>
2013-03-25 15:41:57 +0800

    merged with frontend

---
+++ EzXpns/src/ezxpns/GUI/RecordListView.java
@@ -0,0 +1,258 @@
+/**
+ * 
+ */
+package ezxpns.GUI;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.util.*;
+
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellRenderer;
+
+import ezxpns.data.records.*;
+
+/**
+ * @author yyjhao
+ *
+ */
+@SuppressWarnings("serial")
+public class RecordListView extends JTable {
+	
+	/**
+	 * The GUI Editor that manages the user side inputs for editing of a Record
+	 */
+	public static interface RecordEditor{
+		/**
+		 * Edit the user indicated Record
+		 * @param record to be Edited
+		 * @param display RecordListView Object GUI display to be updated
+		 */
+		public void edit(Record record, RecordListView display);
+	}
+	
+	private static NumberFormat formatter = NumberFormat.getCurrencyInstance();
+	private static DateFormat dateFormatter = DateFormat.getDateInstance();
+	private static String[] headers = {
+		"Name",
+		"Amount",
+		"Category",
+		"Date"
+	};
+	
+	private class ListModel extends AbstractTableModel {
+
+		@Override
+		public int getColumnCount() {
+			return 4;
+		}
+
+		@Override
+		public int getRowCount() {
+			return records.size();
+		}
+
+		@Override
+		public Object getValueAt(int r, int c) {
+			Record re = records.get(r);
+			switch(c){
+			case 0:
+				return re.getName();
+			case 1:
+				if(re instanceof ExpenseRecord){
+					return "-" + formatter.format(re.getAmount());
+				}else{
+					return " " + formatter.format(re.getAmount());
+				}
+			case 2:
+				return re.getCategory();
+			case 3:
+				return dateFormatter.format(re.getDate());
+			default:
+				return null;
+			}
+		}
+		
+		@Override
+		public String getColumnName(int col){
+			return headers[col];
+		}
+	}
+	
+	/**
+	 * Private Empty Constructor to disallow users from creating it without parameters 
+	 */
+	private RecordListView() {
+		super();
+	}
+	
+	private RecordEditor editor;
+	private RecordHandler rhandler;
+	private List<Record> records = new Vector<Record>();
+	private ListModel model = new ListModel();
+	private JPopupMenu menu;
+	private Record itemSelected;
+	private int rowSelected;
+	private UpdateNotifyee notifyee;
+	
+	public RecordListView(RecordEditor ed, RecordHandler rh, UpdateNotifyee notifyee){
+		this();	// To call the superclass constructor
+		editor = ed;
+		rhandler = rh;
+		this.notifyee = notifyee;
+		setSelectionMode(ListSelectionModel.SINGLE_SELECTION); // Single selection
+		setIntercellSpacing(new Dimension(0, 0));
+		setShowHorizontalLines(false);
+		setShowVerticalLines(false);
+		setShowGrid(false);
+		setModel(model);
+		
+		setFont(new java.awt.Font("Segoe UI", 0, 14)); // #Font
+
+		menu = new JPopupMenu();
+		
+		JMenuItem mntmEdit = new JMenuItem("edit");
+		mntmEdit.setAction(new AbstractAction("edit"){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				editItemAt(rowSelected);
+			}
+			
+		});
+		menu.add(mntmEdit);
+		
+		JMenuItem mntmRemove = new JMenuItem("remove");
+		mntmRemove.setAction(new AbstractAction("remove"){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				deleteItemAt(rowSelected);
+			}
+			
+		});
+		menu.add(mntmRemove);
+		
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(arg0.getClickCount() == 2 && arg0.getButton() == MouseEvent.BUTTON1){
+					editItemAt(rowAtPoint(arg0.getPoint()));
+				}
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e){
+		        if (e.isPopupTrigger())
+		            doPop(e);
+		    }
+
+			@Override
+		    public void mouseReleased(MouseEvent e){
+		        if (e.isPopupTrigger())
+		            doPop(e);
+		    }
+
+		    private void doPop(MouseEvent e){
+		    	int row = rowAtPoint(e.getPoint());
+		    	rowSelected = row;
+		    	setRowSelectionInterval(row, row);
+		    	itemSelected = records.get(row);
+				menu.show(e.getComponent(), e.getX(), e.getY());
+		    }
+		});
+	}
+
+	@Override
+	public Component prepareRenderer(TableCellRenderer renderer, int rowIndex, int vColIndex) {
+		Component c = super.prepareRenderer(renderer, rowIndex, vColIndex);
+		if(isCellSelected(rowIndex, vColIndex)) { 	// Default highlighting for user selection
+			return c;
+		}
+		
+		if (rowIndex % 2 == 1 && !isCellSelected(rowIndex, vColIndex)) {
+			c.setBackground(new Color(230, 230, 230));
+		} else {
+			c.setBackground(Color.WHITE); 			// Even rows background color
+		}
+		
+		return c;
+	}
+	
+	/**
+	 * Displays the records
+	 * @param records
+	 */
+	public void show(List<Record> records){
+		this.records = records;
+		model.fireTableDataChanged();
+	}
+
+	/**
+	 * Callback method for edit
+	 * <br />Null return means that the user has cancelled the edit
+	 * @param row
+	 */
+	public void itemEdited(Record newItem){
+		System.out.println(newItem);
+		if(newItem == null)return;
+		records.set(rowSelected, newItem);
+		model.fireTableRowsUpdated(rowSelected, rowSelected);
+		notifyee.updateAll();
+		System.out.println("edited");
+	}
+	
+	/**
+	 * Edit the selected record 
+	 * @param row the row Id of the Record that user has indicated for editing
+	 */
+	protected void editItemAt(int row){
+		rowSelected = row;
+		itemSelected = records.get(row);
+		editor.edit(itemSelected, this);
+	}
+	
+	/**
+	 * Removed the selected record
+	 * @param row the row Id of the Record that user has indicated for removal
+	 */
+	protected void deleteItemAt(int row){
+		String message = "Are you sure you want to remove this record?";
+		if(JOptionPane.showConfirmDialog(this, message, "what?!",
+				JOptionPane.WARNING_MESSAGE, JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION){
+			Record original = records.get(row);
+			if(original instanceof IncomeRecord){
+				original = ((IncomeRecord)original).copy();
+			}else{
+				original = ((ExpenseRecord)original).copy();
+			}
+			rhandler.removeRecord(records.get(row).getId());
+			records.remove(row);
+			model.fireTableRowsDeleted(row, row);
+			notifyee.updateAll();
+			notifyee.addUndoAction(getUndoDeleteRecord(original), "Removing Record");
+		}
+	}
+	
+	private AbstractAction getUndoDeleteRecord(final Record original){
+		return new AbstractAction(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if(original instanceof IncomeRecord){
+					rhandler.createRecord((IncomeRecord)original, false);
+				}else{
+					rhandler.createRecord((ExpenseRecord)original, false, false);
+				}
+			}
+			
+		};
+	}
+}
---
+++ EzXpns/src/ezxpns/data/records/ExpenseRecordManager.java
@@ -0,0 +1,138 @@
+package ezxpns.data.records;
+
+import java.util.*;
+
+
+/**
+ * A special record manager for expense records, since we need to store
+ * all payment methods for it <br />
+ * To be refactor soon
+ * @author yyjhao
+ */
+public class ExpenseRecordManager extends RecordManager<ExpenseRecord>
+	implements PaymentHandler{
+	
+	private TreeMap<Long, PaymentMethod> payms = new TreeMap<Long, PaymentMethod>();
+	private transient TreeMap<Long, TreeSet<ExpenseRecord>> recordsByPaym;
+	
+	public ExpenseRecordManager(){
+		super();
+		recordsByPaym = new TreeMap<Long, TreeSet<ExpenseRecord>>();
+	}
+
+	@Override
+	public void afterDeserialize(){
+		super.afterDeserialize();
+		recordsByPaym = new TreeMap<Long, TreeSet<ExpenseRecord>>();
+		for(ExpenseRecord r : recordsById.values()){
+			r.paymentMethod = payms.get(r.paymentMethod.id);
+			TreeSet<ExpenseRecord> rs = recordsByPaym.get(r.paymentMethod.id);
+			if(rs == null){
+				rs = new TreeSet<ExpenseRecord>();
+				recordsByPaym.put(r.paymentMethod.id, rs);
+			}
+			rs.add(r);
+		}
+		payms.remove(PaymentMethod.undefined.id);
+		payms.put(PaymentMethod.undefined.id, PaymentMethod.undefined);
+	}
+	
+	@Override
+	protected void recordAdded(ExpenseRecord r){
+		super.recordAdded(r);
+		TreeSet<ExpenseRecord> rs = recordsByPaym.get(r.paymentMethod.id);
+		if(rs == null){
+			rs = new TreeSet<ExpenseRecord>();
+			recordsByPaym.put(r.paymentMethod.id, rs);
+		}
+		rs.add(r);
+	}
+	
+	@Override
+	protected void recordRemoved(ExpenseRecord r){
+		super.recordRemoved(r);
+		recordsByPaym.get(r.paymentMethod.id).remove(r);
+	}
+	
+	@Override
+	public Vector<PaymentMethod> getAllPaymentMethod() {
+		Vector<PaymentMethod> pms = new Vector<PaymentMethod>();
+		for(PaymentMethod p : payms.values()){
+			if(p != PaymentMethod.undefined){
+				pms.add(p);
+			}
+		}
+		return pms;
+	}
+
+	@Override
+	public PaymentMethod addNewPaymentMethod(PaymentMethod paymentRef) {
+		if(validatePaymentMethodName(paymentRef.name) != null){
+			return null;
+		}
+		paymentRef = paymentRef.copy();
+		while(payms.containsKey(paymentRef.id)){
+			paymentRef.id = (new Date()).getTime() + ran.nextInt();
+		}
+		payms.put(paymentRef.id, paymentRef);
+		markUpdate();
+		return paymentRef;
+	}
+
+	@Override
+	public boolean removePaymentMethod(long id) {
+		if(!payms.containsKey(id)){
+			return false;
+		}else{
+			payms.remove(id);
+			if(recordsByPaym.get(id) != null){
+				TreeSet<ExpenseRecord> rs = recordsByPaym.get(PaymentMethod.undefined.id);
+				for(ExpenseRecord r : recordsByPaym.get(id)){
+					r.paymentMethod = PaymentMethod.undefined;
+					rs.add(r);
+				}
+				recordsByPaym.remove(id);
+			}
+			markUpdate();
+			return true;
+		}
+	}
+
+	@Override
+	public PaymentMethod updatePaymentMethod(long id, PaymentMethod paymentRef) {
+		if(!payms.containsKey(id)){
+			return null;
+		}else{
+			PaymentMethod p = payms.get(id);
+			if(p.name.equals(paymentRef.name)){
+				return p;
+			}
+			if(validatePaymentMethodName(paymentRef.name) != null){
+				return null;
+			}
+			p.name = paymentRef.name;
+			markUpdate();
+			return p;
+		}
+	}
+
+	@Override
+	public String validatePaymentMethodName(String name) {
+		if(name.length() < 2)return "Payment Method name is too short";
+		if(name.length() > 20)return "Payment Method name is too long";
+		if(name.contains(" "))return "Payment Method name should not contain spaces";
+		if(containsPaymentMethodName(name))return "Payment Method name is used.";
+		return null;
+	}
+
+	private boolean containsPaymentMethodName(String name) {
+		for(PaymentMethod p : payms.values()){
+			if(p.name.equals(name)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	
+}
