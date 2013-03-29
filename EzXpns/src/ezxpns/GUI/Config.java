2ac1bd020c6cdb1a2c4b2d5c5e48604ef68eb801
kohzk <kohzk@comp.nus.edu.sg>
2013-03-29 13:18:41 +0800

    Breaking apart to form experimental modular window gui

---
+++ EzXpns/src/ezxpns/GUI/Config.java
@@ -0,0 +1,16 @@
+package ezxpns.GUI;
+
+import java.awt.Font;
+
+public class Config {
+	
+	public static final int DEFAULT_MENU_WIDTH = 134;
+	public static final int DEFAULT_MENU_HEIGHT = 30;
+	
+	
+	public static final Font TITLE_FONT = new Font("", 0, 24);
+	public static final Font MENU_FONT = new Font("", 0, 18);
+	public static final Font TEXT_FONT = new Font("", 0, 14);
+	
+	private Config(){}
+}
--- EzXpns/src/ezxpns/GUI/EzNavigator.java
+++ EzXpns/src/ezxpns/GUI/EzNavigator.java
@@ -2,7 +2,16 @@ package ezxpns.GUI;
 
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JLayeredPane;
 
 /**
@@ -11,51 +20,161 @@ import javax.swing.JLayeredPane;
 @SuppressWarnings("serial")
 public class EzNavigator extends JLayeredPane {
 
-	// TODO: Navigator Menus (A list of functions/links)
-	// TODO: Incorporate Simple Search 
-	// TODO: Manipulate the main gui panels
+	/**
+	 * CardLayout to manage the different content panels
+	 */
+	private CardLayout contentMgr;
 	
-	private CardLayout loCtrl;
+	/**
+	 * The parent content panel that will do the switching of panels
+	 */
 	private JLayeredPane content;
 	
+	private JButton btnUndo;
+	
 	private EzNavigator() {
-		this.setBackground(Color.BLACK);
-		// TODO: Create a Accordion Menu :) if possible
-		// TODO: Link the menu buttons to the appropriate link
+		super();
+		this.setBounds(new Rectangle (0, 0, 134, 600));
+		// TODO: Incorporate Simple Search Field [?]
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.BOTH;
+		// gbc.weighty = 1;
+		gbc.weightx = 1;
+		JButton btn = createMenuBtn(MenuOption.SEARCH);
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		this.add(btn, gbc);
+
+		btn = createMenuBtn(MenuOption.REVERT);
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		btnUndo = btn; // Stored for cosmetic updates
+		this.add(btn, gbc);
+		
+		btn = createMenuBtn(MenuOption.NEWEXP);
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		this.add(btn, gbc);
+		
+		btn = createMenuBtn(MenuOption.NEWINC);
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+		this.add(btn, gbc);
+		
+		btn = createMenuBtn(MenuOption.DASHBD);
+		gbc.gridx = 0;
+		gbc.gridy = 5;
+		this.add(btn, gbc);
+
+		btn = createMenuBtn(MenuOption.CATMGR);
+		gbc.gridx = 0;
+		gbc.gridy = 6;
+		this.add(btn, gbc);
+		
+		btn = createMenuBtn(MenuOption.PAYMGR);
+		gbc.gridx = 0;
+		gbc.gridy = 7;
+		this.add(btn, gbc);
+		
+		btn = createMenuBtn(MenuOption.REPORT);
+		gbc.gridx = 0;
+		gbc.gridy = 8;
+		this.add(btn, gbc);
+
+		// TODO: Manipulate the main gui panels
+		
 		// TODO: Setup the layout properly for the menu buttons
-		// TODO: SUBMENU Hiding - Categorizing Menu options, etc 
+		
+		// TODO: SUBMENU Hiding - Categorizing Menu options, etc
+
 	}
 	
-	public EzNavigator(CardLayout loCtrl, JLayeredPane contentPane) {
+	/**
+	 * Constructs a Navigator
+	 * @param contentMgrRef CardLayout object that will manage the content switching
+	 * @param contentPane JLayeredPane object that will be hosting all the content (content Placeholder)
+	 */
+	public EzNavigator(CardLayout contentMgrRef, JLayeredPane contentPane) {
 		this();
-		this.loCtrl = loCtrl;
+		this.contentMgr = contentMgrRef;
 		this.content = contentPane;
 	}
 	
-	public void navigate(MenuCard card) {
-		loCtrl.show(content, card.toString());
+	/**
+	 * Navigates the content panel
+	 * @param card MenuCard to be called
+	 */
+	public void navigate(MenuOption card) {
+		contentMgr.show(content, card.toString());
+	}
+	
+	/**
+	 * Creates a Menu button for the navigator
+	 * @param card MenuCard Object to tag button to
+	 * @return a JButton object tagged to the given MenuCard.
+	 */
+	private JButton createMenuBtn(final MenuOption card) {
+		JButton btn = new JButton(card.toString());
+		btn.setContentAreaFilled(false);
+		btn.setBorder(BorderFactory.createRaisedBevelBorder());
+		
+		btn.setFont(Config.MENU_FONT);
+		btn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				navigate(card);
+			}
+		});
+		btn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent mEvent) { // Hover start
+				JButton btn = (JButton) mEvent.getSource();
+				btn.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), BorderFactory.createLineBorder(Color.cyan)));
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent mEvent) {
+				JButton btn = (JButton) mEvent.getSource();
+				btn.setBorder(BorderFactory.createLoweredBevelBorder());
+			}
+			
+			@Override
+			public void mouseReleased(MouseEvent mEvent) {
+				JButton btn = (JButton) mEvent.getSource();
+				btn.setBorder(BorderFactory.createRaisedBevelBorder());
+			}
+			
+			public void mouseExited(MouseEvent mEvent) { // Hover end
+				JButton btn = (JButton) mEvent.getSource();
+				btn.setBorder(BorderFactory.createRaisedBevelBorder());
+			}
+		});
+		return btn;
 	}
 }
 
 /**
  * Menu Names for labeling and operating cards
  */
-enum MenuCard {
+enum MenuOption {
 	
-	NEWRCD 	("New Record"),
-	NEWINC 	("New Income Record"),
+	NEWRCD 	("New Record"), 			// Maybe this isn't an option
+	NEWINC 	("New Income Record"),		
 	NEWEXP 	("New Expense Record"),
-	SEARCH 	("Search"),
-	CATMGR 	("Manage Category"),
-	PAYMGR 	("Manage Payments"),
-	TARGET 	("Manage Targets"),
-	REPORT 	("Generate Report"),
+	SEARCH 	("Search"),					// TODO: Advanced Search ?
+	CATMGR 	("Manage Category"),		
+	PAYMGR 	("Manage Payments"),		// TODO: Remove or keep?
+	TARGET 	("Manage Targets"),			// TODO: Is that the one to keep?
 	DASHBD 	("Dashboard"),
-	UNDO	("Undo");
+	CONFIG	("Configurations"),
+	REVERT	("Undo"),
+	REPORT 	("Generate Report");
 	
 	public final String name;
-	private MenuCard(String name) {this.name = name;}
+	private MenuOption(String name) {this.name = name;}
 	
+	@Override
 	public String toString() {
 		return this.name;
 	}
