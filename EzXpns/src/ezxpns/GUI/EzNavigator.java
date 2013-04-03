577516948d4c9af43339bb3c6e34962473594513
kohzk <kohzk@comp.nus.edu.sg>
2013-04-03 14:03:42 +0800

    Added the formatting for the overview panel

---
+++ EzXpns/src/ezxpns/GUI/EzNavigator.java
@@ -0,0 +1,339 @@
+package ezxpns.GUI;
+
+import java.awt.CardLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLayeredPane;
+
+/**
+ * Navigator for the MainGUI
+ */
+@SuppressWarnings("serial")
+public class EzNavigator extends JLayeredPane {
+
+	/**
+	 * CardLayout to manage the different content panels
+	 */
+	private CardLayout contentMgr;
+	
+	/**
+	 * The parent content panel that will do the switching of panels
+	 */
+	private JLayeredPane content;
+	
+	private UIControl uiCtrl;
+	
+	private JButton btnUndo;
+	private JButton selected;
+	
+	private EzNavigator(UIControl uiCtrl) {
+		super();
+		this.uiCtrl = uiCtrl;
+		this.setBounds(new Rectangle (0, 0, 80, 600));
+		this.setDoubleBuffered(true);
+		// TODO: Incorporate Simple Search Field [?]
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.insets = new Insets(5, 15, 5, 15);
+		// gbc.weighty = 1;
+		gbc.weightx = 0.5;
+		JButton btn = createMenuBtn(NormalMenuOpt.SEARCH);
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		this.add(btn, gbc);
+
+		btn = createMenuBtn(NormalMenuOpt.REVERT);
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		btnUndo = btn; // Stored for cosmetic updates
+		this.add(btn, gbc);
+		btnUndo.setAction(uiCtrl.getUndoMgr().getAction());
+		(new Thread() {
+			@Override
+			public void run() {
+				while(true) {
+					btnUndo.setBorderPainted(btnUndo.isEnabled());
+				}
+			}
+		}).start();
+		
+		btn = createMenuBtn(new NewExpenseDialog(this.uiCtrl));
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		this.add(btn, gbc);
+		
+		btn = createMenuBtn(new NewIncomeDialog(this.uiCtrl));
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+		this.add(btn, gbc);
+		
+		btn = createMenuBtn(NormalMenuOpt.DASHBD);
+		gbc.gridx = 0;
+		gbc.gridy = 5;
+		this.add(btn, gbc);
+		selected = btn;
+
+		btn = createMenuBtn(NormalMenuOpt.CATMGR);
+		gbc.gridx = 0;
+		gbc.gridy = 6;
+		this.add(btn, gbc);
+		
+		btn = createMenuBtn(new ReportDialog(this.uiCtrl));
+		gbc.gridx = 0;
+		gbc.gridy = 7;
+		this.add(btn, gbc);
+		
+//		btn = createMenuBtn(NormalMenuOpt.PAYMGR);
+//		gbc.gridx = 0;
+//		gbc.gridy = 7;
+//		this.add(btn, gbc);
+
+		// TODO: Manipulate the main gui panels
+		
+		// TODO: Setup the layout properly for the menu buttons
+		
+		// TODO: SUBMENU Hiding - Categorizing Menu options, etc
+		
+
+	}
+	
+	/**
+	 * Constructs a Navigator
+	 * @param contentMgrRef CardLayout object that will manage the content switching
+	 * @param contentPane JLayeredPane object that will be hosting all the content (content Placeholder)
+	 */
+	public EzNavigator(UIControl uiCtrl, CardLayout contentMgrRef, JLayeredPane contentPane) {
+		this(uiCtrl);
+		this.contentMgr = contentMgrRef;
+		this.content = contentPane;
+	}
+	
+	/**
+	 * Navigates the content panel
+	 * @param card MenuCard to be called
+	 */
+	public void navigate(NormalMenuOpt option) {
+		contentMgr.show(content, option.toString());
+	}
+	
+	/**
+	 * Opens a new dialog
+	 * @param option
+	 */
+	public void navigate(DialogMenuOpt option) {
+		option.openDialog();
+	}
+	
+	/**
+	 * 
+	 * @param btn
+	 * @param card
+	 */
+	public void linkNavi(JButton btn, MenuOption card) {
+		if(card instanceof NormalMenuOpt) {
+			linkNormalMenuOpt(btn, (NormalMenuOpt) card);
+		}
+		if(card instanceof DialogMenuOpt) {
+			linkDiagMenuOpt(btn, (DialogMenuOpt) card);
+		}
+	}
+	
+	
+	/**
+	 * @param btn
+	 * @param option
+	 */
+	public void linkDiagMenuOpt(JButton btn, final DialogMenuOpt option) {
+		btn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				navigate(option);
+			}
+		});
+	}
+	
+	/**
+	 * 
+	 * @param btn
+	 * @param option
+	 */
+	public void linkNormalMenuOpt(JButton btn, final NormalMenuOpt option) {
+		btn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				navigate(option);
+			}
+		});
+	}
+	
+	/**
+	 * Creates a Menu button for the navigator
+	 * @param card MenuCard Object to tag button to
+	 * @return a JButton object tagged to the given MenuCard.
+	 */
+	private JButton createMenuBtn(NormalMenuOpt option) {
+		JButton btn = new JButton(option.toString());
+		btn.setContentAreaFilled(false);
+		btn.setBorder(BorderFactory.createRaisedBevelBorder());
+		btn.setFocusPainted(false);
+		btn.setFont(Config.MENU_FONT);
+		linkNavi(btn, option);
+		btn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent mEvent) {
+				JButton btn = (JButton) mEvent.getSource();
+				if(btn != selected)
+					btn.setBorder(BorderFactory.createLoweredBevelBorder());
+			}
+			
+			@Override
+			public void mouseReleased(MouseEvent mEvent) {
+				JButton btn = (JButton) mEvent.getSource();
+				if(btn != selected) {
+					selected.setBorder(BorderFactory.createRaisedBevelBorder());
+					selected = btn;
+				}
+			}
+			
+			public void mouseExited(MouseEvent mEvent) { // Hover end
+				JButton btn = (JButton) mEvent.getSource();
+				if(btn != selected)
+					btn.setBorder(BorderFactory.createRaisedBevelBorder());
+			}
+		});
+		return btn;
+	}
+	
+	/**
+	 * Creates a JButton for the navigator that will open dialogs
+	 * @param option
+	 * @return
+	 */
+	private JButton createMenuBtn(DialogMenuOpt option) {
+		JButton btn = new JButton(option.toString());
+		btn.setContentAreaFilled(false);
+		btn.setBorder(BorderFactory.createRaisedBevelBorder());
+		btn.setFocusPainted(false);
+		btn.setFont(Config.MENU_FONT);
+		linkNavi(btn, option);
+		btn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent mEvent) {
+				JButton btn = (JButton) mEvent.getSource();
+				btn.setBorder(BorderFactory.createLoweredBevelBorder());
+			}
+			
+			@Override
+			public void mouseReleased(MouseEvent mEvent) {
+				JButton btn = (JButton) mEvent.getSource();
+				btn.setBorder(BorderFactory.createRaisedBevelBorder());
+			}
+		});
+		return btn;
+	}
+}
+
+/**
+ * Generic Menu Options
+ */
+interface MenuOption {}
+
+/**
+ *
+ */
+enum NormalMenuOpt implements MenuOption {
+	
+	NEWRCD 	("New Record"), 			// Maybe this isn't an option
+	SEARCH 	("Search"),					// TODO: Advanced Search ?
+	CATMGR 	("Manage Category"),		
+	PAYMGR 	("Manage Payments"),		// TODO: Remove @Yujian
+	TARGET 	("Manage Targets"),			// TODO: Is that the one to keep?
+	DASHBD 	("Dashboard"),
+	CONFIG	("Configurations"),			// Further implementation
+	REVERT	("Undo");
+	
+	
+	public final String name;
+	private NormalMenuOpt(String name) {this.name = name;}
+	
+	@Override
+	public String toString() {
+		return this.name;
+	}
+}
+
+/**
+ * 
+ */
+abstract class DialogMenuOpt implements MenuOption {
+	
+	protected String name;
+	protected UIControl uiCtrl;
+	
+	public DialogMenuOpt(String name, UIControl uiCtrlRef) {
+		this.name = name;
+		this.uiCtrl = uiCtrlRef;
+	}
+	
+	public String toString() {
+		return this.name;
+	}
+	public abstract void openDialog();
+}
+
+
+/**
+ *
+ */
+class NewIncomeDialog extends DialogMenuOpt {
+	
+	public NewIncomeDialog(UIControl uiCtrl) {
+		super("New Income", uiCtrl);
+	}
+
+	@Override
+	public void openDialog() {
+		// TODO Auto-generated method stub
+		uiCtrl.showRecWin(RecordDialog.TAB_INCOME);
+	}	
+}
+
+/**
+ *
+ */
+class NewExpenseDialog extends DialogMenuOpt {
+	
+	public NewExpenseDialog(UIControl uiCtrl) {
+		super("New Expense", uiCtrl);
+	}
+
+	@Override
+	public void openDialog() {
+		uiCtrl.showRecWin(RecordDialog.TAB_EXPENSE);
+	}
+}
+
+/**
+ *
+ */
+class ReportDialog extends DialogMenuOpt {
+	
+	public ReportDialog(UIControl uiCtrl) {
+		super("Generate Report", uiCtrl);
+	}
+
+	@Override
+	public void openDialog() {
+		uiCtrl.showReportWin();
+	}
+}
---
+++ EzXpns/src/ezxpns/GUI/MainGUI.java
@@ -0,0 +1,160 @@
+package ezxpns.GUI;
+
+import java.awt.CardLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.AbstractAction;
+import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+
+import ezxpns.data.NWSGenerator;
+import ezxpns.data.SummaryGenerator;
+import ezxpns.data.TargetManager;
+import ezxpns.data.records.CategoryHandler;
+import ezxpns.data.records.ExpenseRecord;
+import ezxpns.data.records.IncomeRecord;
+import ezxpns.data.records.PaymentHandler;
+import ezxpns.data.records.SearchHandler;
+
+@SuppressWarnings("serial")
+public class MainGUI extends JFrame implements UpdateNotifyee {
+	
+	private UIControl uiCtrl;
+	private UndoManager undoMgr;
+	
+	/**
+	 * Main Navigator for UI
+	 */
+	private EzNavigator navi;
+	private JLayeredPane panContent;
+	
+	/**
+	 * 
+	 */
+	private SavingsOverviewPanel panSavings;
+	private OverviewPanel panOverview;
+	private TargetOverviewPanel panTarget;
+	
+	private SearchFrame panSearch;
+	private CategoryFrame panCategory;
+	
+	public MainGUI(
+			NWSGenerator nwsGen,
+			SummaryGenerator sumGen, 
+			TargetManager targetMgr, 
+			UIControl uiCtrl,
+			UndoManager undoMgr) {
+		super();
+		this.instantiateUI();
+		this.undoMgr = undoMgr;
+		
+		this.uiCtrl = uiCtrl;
+		// TODO: InstantiateNavi
+		// TODO: CardLayout
+		// TODO: InstantiateContent
+		// TODO: Iterate through the list of JLayeredPane/JPanel to be displayed
+		
+		
+		
+		CardLayout contentMgr = new CardLayout();
+		panContent = new JLayeredPane();
+		panContent.setLayout(contentMgr);
+		navi = new EzNavigator(uiCtrl, contentMgr, panContent);
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		// Setup for Navigator
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weightx = 0.1;
+		gbc.weighty = 1;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		this.add(navi, gbc);
+		// Setup for Content
+		
+		JPanel homeContent = new JPanel(new GridBagLayout());
+		GridBagConstraints gbContent = new GridBagConstraints();
+		
+		gbContent.gridx = 0;
+		gbContent.gridy = 0;
+		gbContent.fill = GridBagConstraints.HORIZONTAL;
+		gbContent.weightx = 1;
+		gbContent.weighty = 0.5;
+		gbContent.gridwidth = 2;
+		gbContent.gridheight = 1;
+		panOverview = new OverviewPanel(sumGen);
+		homeContent.add(panOverview, gbContent);
+		
+		gbContent.fill = GridBagConstraints.BOTH;
+		gbContent.gridwidth = 1;
+		gbContent.gridheight = 3;
+		gbContent.weightx = 0.5;
+		gbContent.weighty = 1;
+		gbContent.gridy = 1;
+		panTarget = new TargetOverviewPanel(targetMgr);
+		homeContent.add(panTarget, gbContent);
+		
+		gbContent.gridx = 1;
+		panSavings = new SavingsOverviewPanel(nwsGen);
+		homeContent.add(panSavings, gbContent);
+		
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weightx = 0.9;
+		gbc.weighty = 1;
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		
+		panContent.add(homeContent, NormalMenuOpt.DASHBD.toString());
+		
+		this.add(panContent, gbc);
+		this.setVisible(true);
+	}
+	
+	private void instantiateUI() {
+		this.setBounds(0, 0, Config.DEFAULT_UI_WIDTH, Config.DEFAULT_UI_HEIGHT);
+		this.setLocationRelativeTo(null);
+		this.setLayout(new GridBagLayout());
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		// this.setUndecorated(true); // Remove Title Bar 
+	}
+	
+	public void loadCategoryPanel(
+			CategoryHandler<ExpenseRecord> expenseHandler, 
+			CategoryHandler<IncomeRecord> incomeHandler, 
+			TargetManager targetMgr) {
+		if(panCategory==null) {
+			panCategory = new CategoryFrame(expenseHandler, incomeHandler, targetMgr, this);
+			panContent.add(panCategory, NormalMenuOpt.CATMGR.toString());
+		}
+	}
+	
+	public void loadSearchPanel(
+			SearchHandler searchHandler, 
+			RecordListView display, 
+			CategoryHandler<IncomeRecord> incomeHandler, 
+			CategoryHandler<ExpenseRecord> expenseHandler, 
+			PaymentHandler payHandler) {
+		if(panSearch == null) {
+			panSearch = new SearchFrame(searchHandler, display, incomeHandler, expenseHandler, payHandler);
+			panContent.add(panSearch, NormalMenuOpt.SEARCH.toString());
+		}
+	}
+
+	@Override
+	public void updateAll() {
+		panTarget.update();
+		panSavings.update();
+		panOverview.updateOverview();
+		panOverview.validate();
+		panCategory.reload();
+		this.validate();
+	}
+
+	@Override
+	public void addUndoAction(AbstractAction action, String name) {
+		undoMgr.add(action, name);
+	}
+}
