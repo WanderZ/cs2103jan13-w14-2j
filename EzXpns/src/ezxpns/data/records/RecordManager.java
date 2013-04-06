1959cee1a9e8fc1fc6e4578af005575873b9beb9
Yao Yujian yyjhao <Yao Yujian yyjhao@gmail.com>
2013-04-06 16:22:55 +0800

    time stuff eh

--- EzXpns/src/ezxpns/data/records/RecordManager.java
+++ EzXpns/src/ezxpns/data/records/RecordManager.java
@@ -280,6 +280,9 @@ public class RecordManager<T extends Record>
 	public T addNewRecord(T toAdd) throws RecordUpdateException{
 		T record = (T)toAdd.copy();
 		record.category = categories.get(toAdd.category.getID());
+		if(!toAdd.date.before(today)){
+			toAdd.date = new Date();
+		}
 		if(record.category == null){
 			throw new RecordUpdateException("Invalid category!");
 		}
