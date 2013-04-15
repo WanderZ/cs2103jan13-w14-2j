f6e1359d7f0ba4324c2c03c485e754ea423bd13f
ytz <tingzhe@gmail.com>
2013-04-15 18:04:15 +0800

    trying to merge good lord

---
+++ EzXpns/src/test/ReportGeneratorTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReportGeneratorTest {
+
+	@Test
+	public void testReportGenerator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGenerateReport() {
+		fail("Not yet implemented");
+	}
+
+}
